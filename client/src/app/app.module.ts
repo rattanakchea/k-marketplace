2bfc8801f94e95551d17c1138fe8b09b68399807
Rattanak Chea <rattanak.chea@premera.com>
2017-09-21 23:51:55 -0700

    added HTTP module, testing api service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { ApiService } from './services/api.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
