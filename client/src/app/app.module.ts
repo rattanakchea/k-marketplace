e0e9c258bb5ddb3f1202c1cb75fc0bb14b8aa5f6
Rattanak Chea <rattanak.chea@premera.com>
2017-09-21 23:06:56 -0700

    added homecomponent prefix with k-

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopnavComponent } from './topnav/topnav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopnavComponent
+    TopnavComponent,
+    SidebarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
