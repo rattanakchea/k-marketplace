974b3ec8f8225b8e29091d95fbc6071ce06b83e7
Rattanak Chea <rattanak.chea@premera.com>
2017-09-21 22:57:11 -0700

    added bootstrap scss, dashboard template, topnav component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopnavComponent } from './topnav/topnav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent
   ],
   imports: [
     BrowserModule,
